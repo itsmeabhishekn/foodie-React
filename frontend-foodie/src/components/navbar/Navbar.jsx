2c034eacfa1fb0f0e21e88446ab747cf65d9d6ed
itsmeabhishekn <abhishekn4454@gmail.com>
2024-08-06 23:37:07 +0530

    add to and remove from cart is implemented

--- frontend-foodie/src/components/navbar/Navbar.jsx
+++ frontend-foodie/src/components/navbar/Navbar.jsx
@@ -1,28 +1,50 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import {Link} from 'react-router-dom'
 
-const Navbar = ({setShowLogin}) => {
-
-    const [menu,setMenu] = useState("home")
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
 
   return (
     <div className="navBar">
-      <img src={assets.logo} className="logo" alt=""  />
+    <Link to={'/'}>  <img src={assets.logo} className="logo" alt="" /> </Link>
       <ul className="navbar-menu">
-        <li onClick={()=> setMenu("home")} className={menu === "home" ? "active":"" }>home</li>
-        <li onClick={()=> setMenu("menu")} className={menu === "menu" ? "active" : "" }>menu</li>
-        <li onClick={()=> setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : "" }>mobile-app</li>
-        <li onClick={()=> setMenu("contact-us")} className={menu === "contact-us" ? "active" : "" }>contact us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
       </ul>
 
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
           <div className="dot"></div>
         </div>
-          <button onClick={()=>setShowLogin(true)}>Sign In</button>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
       </div>
     </div>
   );
